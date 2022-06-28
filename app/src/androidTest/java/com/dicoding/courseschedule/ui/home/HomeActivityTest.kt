7cd43dadd34380682c054c5f545e950f6fb84eb4
Abraham Naiborhu <87258755+abrahamparn@users.noreply.github.com>
2022-06-28 23:04:46 +0700

    Everything

---
+++ app/src/androidTest/java/com/dicoding/courseschedule/ui/home/HomeActivityTest.kt
@@ -0,0 +1,43 @@
+package com.dicoding.courseschedule.ui.home
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.dicoding.courseschedule.R
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class HomeActivityTest{
+
+    @Before
+    fun setup(){
+        ActivityScenario.launch(HomeActivity::class.java)
+    }
+
+    @Test
+    fun testActivity(){
+        onView(withId(R.id.action_add)).perform(click())
+
+        onView(withId(R.id.add_name_course)).check(matches(isDisplayed()))
+        onView(withId(R.id.add_course_day)).check(matches(isDisplayed()))
+        onView(withId(R.id.imageButton)).check(matches(isDisplayed()))
+        onView(withId(R.id.add_name_course)).check(matches(isDisplayed()))
+        onView(withId(R.id.textView)).check(matches(isDisplayed()))
+        onView(withId(R.id.add_course_start)).check(matches(isDisplayed()))
+        onView(withId(R.id.imageButton2)).check(matches(isDisplayed()))
+        onView(withId(R.id.textView2)).check(matches(isDisplayed()))
+        onView(withId(R.id.add_course_end)).check(matches(isDisplayed()))
+
+
+    }
+}
---
+++ app/src/main/java/com/dicoding/courseschedule/ui/add/AddCourseActivity.kt
@@ -0,0 +1,85 @@
+package com.dicoding.courseschedule.ui.add
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import com.dicoding.courseschedule.R
+import com.dicoding.courseschedule.databinding.ActivityAddCourseBinding
+import com.dicoding.courseschedule.ui.list.ListViewModelFactory
+import com.dicoding.courseschedule.util.TimePickerFragment
+import java.text.SimpleDateFormat
+import java.util.*
+
+class AddCourseActivity : AppCompatActivity(),  TimePickerFragment.DialogTimeListener {
+    private lateinit var viewModel: AddCourseViewModel
+    private lateinit var binding: ActivityAddCourseBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddCourseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = getString(R.string.add_course)
+
+        setupModel()
+    }
+
+    private fun setupModel() {
+        viewModel = ViewModelProvider(
+            this,
+            ListViewModelFactory.createFactory(this)
+        )[AddCourseViewModel::class.java]
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_add, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_insert -> {
+
+                viewModel.insertCourse(
+                    binding.addNameCourse.text.toString().trim(),
+                    binding.addCourseDay.selectedItemPosition,
+                    binding.addCourseStart.text.toString().trim(),
+                    binding.addCourseEnd.text.toString().trim(),
+                    binding.addCourseLecturer.text.toString().trim(),
+                    binding.addCourseNote.text.toString().trim()
+                )
+                true
+            }else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun showTimePickerStartTime(view: View) {
+        TimePickerFragment().show(supportFragmentManager, "startPicker")
+    }
+
+    fun showTimePickerEndTime(view: View) {
+        TimePickerFragment().show(supportFragmentManager, "endPicker")
+    }
+
+    override fun onDialogTimeSet(tag: String?, hour: Int, minute: Int) {
+        val calendar = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, hour)
+            set(Calendar.MINUTE, minute)
+        }
+
+
+        val dateFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+
+        if (tag == "startPicker") {
+            binding.addCourseStart.text = dateFormat.format(calendar.time)
+        } else {
+            binding.addCourseEnd.text = dateFormat.format(calendar.time)
+        }
+    }
+
+
+}
